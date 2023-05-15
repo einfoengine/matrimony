69b29e3876a6ef63869bc708d1e6c28d75b5662a
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-05-15 18:02:38 +0800

    Added tost

--- client/components/MainNav/index.tsx
+++ client/components/MainNav/index.tsx
@@ -4,22 +4,36 @@ import { LoginContext } from "../../context";
 import styles from "../../styles/MainNav.module.css";
 import axios from "axios";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const index = () => {
   const {state} = useContext(LoginContext);
   const [user, setUser] = useState();
+  // const router = useRouter();
+
   useEffect(()=>{
     axios.get(`http://localhost:3000/api/users/user/${state?.user?._id}`).then((res)=>{
       setUser(res?.data);
     });
   },[state]);
+
+  const notify = () => toast("To find partner, login first!");
   return (
     <>
+    {console.log("Main menu: ", user)}
       <nav className={styles.exbg}>
-        {console.log("from menu", user)}
         <Link href={'/'}><a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">Home</a></Link>
         {/* {state.user && <Link href={`/biodata?user=${state?.user?._id}`}><a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">Biodata</a></Link>} */}
-        {state.user?.biodata === "complete" && <Link href={'/users'}><a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">Users</a></Link>}
+        {/* {state.user?.biodata === "complete" && <Link href={'/users'}><a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">Users</a></Link>} */}
+        {
+          user?._id?
+          <Link href={'/users'}><a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">See biodatas</a></Link>
+          :
+          <a onClick={notify} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">See biodatas</a>
+        }
       </nav>
+      <ToastContainer />
     </>
   )
 }
